92684049ffcba2569aec0ab1b155bb6a5b348fbb
Laurentiu Popa <popa.laur.01@gmail.com>
2023-06-18 23:45:57 +0300

    Implemented the paying

--- client/src/components/Main/Main.jsx
+++ client/src/components/Main/Main.jsx
@@ -1,6 +1,6 @@
 import MainContent from "./MainContent"
 import MainModel from "./MainModel"
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useLocation } from "react-router-dom"
 import { PayPalButtons, PayPalScriptProvider} from '@paypal/react-paypal-js'
 
@@ -9,6 +9,7 @@ const Main = () => {
     const [subject, setSubject] = useState('')
     const location = useLocation()
     const {email, role, paid} = location.state
+    const [p, setP] = useState(paid)
     
     console.log(paid)
 
@@ -16,13 +17,37 @@ const Main = () => {
         setSubject(aux)
     }
 
+    useEffect(() => {
+      
+    },[p])
+
     return (
-      paid === 0 ? (
+      p === 0 ? (
         <div className="main-plata">
           <h1>If you want to use the 
             courses that the teachers are providing you should
             pay a fee of $10 
           </h1>
+          <PayPalScriptProvider options={{"client-id":"AYbapIMjwUbmxI1BTDW0_F6bJ0VoSbpWJ5Rk1_QBdkASecauWOgxB43Jtzp4IPTxVKi2jDpRfHO7DgaB"}}>
+            <PayPalButtons 
+              createOrder={(data, actions) => {
+                return actions.order.create({
+                  purchase_units: [
+                    {
+                      amount: {
+                        value: "10.00",
+                      },
+                    },
+                  ],
+                });
+              }}
+              onApprove={async (data, actions) => {
+                const details = await actions.order.capture();
+                const name = details.payer.name.given_name;
+                setP(1)
+              }}
+            />
+          </PayPalScriptProvider>
         </div>
       ) : (
         <div className="main">
